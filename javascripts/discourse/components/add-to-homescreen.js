92b7441470f6bff0b683ccd127aa2cdd22389cde
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-06-01 11:47:48 +0200

    DEV: various refactorings
    - correctly clear event listener
    - uses discourseLater to ensure if it's ever used in a test we don't wait a full second
    - simplifies arrowUp computation
    - removes not needed `@classNameBindings("shouldRender:show")` as far as I can tell
    - ensures we still have a valid object in later handler

--- javascripts/discourse/components/add-to-homescreen.js
+++ javascripts/discourse/components/add-to-homescreen.js
@@ -1,52 +1,39 @@
 import Component from "@glimmer/component";
-import { classNameBindings } from "@ember-decorators/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 import { later } from "@ember/runloop";
 import I18n from "I18n";
+import discourseLater from "discourse-common/lib/later";
+import { bind } from "discourse-common/utils/decorators";
 
-@classNameBindings("shouldRender:show")
 export default class AddToHomescreen extends Component {
   @service capabilities;
   @service currentUser;
   @service keyValueStore;
   @service site;
-  @tracked hasHiddenPopup;
+
+  @tracked hasHiddenPopup = this.keyValueStore.getItem("hasHiddenPopup");
   @tracked showPopupTimer = false;
   @tracked arrowUp = !window.matchMedia("(orientation: portrait)");
 
   constructor() {
     super(...arguments);
-    this.hasHiddenPopup = this.keyValueStore.getItem("hasHiddenPopup");
 
-    later(() => {
+    discourseLater(() => {
+      if (this.isDestroying || this.isDestroyed) {
+        return;
+      }
+
       this.showPopupTimer = true;
     }, 1000);
-
-    if (this.capabilities.isIpadOS) {
-      this.arrowUp = true;
-      return;
-    }
-
-    window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
-      const portrait = e.matches;
-
-      if (this.capabilities.isIpadOS) {
-        this.arrowUp = true;
-        return;
-      } else if (portrait) {
-          this.arrowUp = false;
-      } else {
-          this.arrowUp = true;
-      }
-  });
   }
 
   get shouldRender() {
     const appleMobile = this.capabilities.isIOS || this.capabilities.isIpadOS;
     const isPWA = this.capabilities.isPwa;
     const isHub = this.capabilities.wasLaunchedFromDiscourseHub;
+
     return (
       this.currentUser &&
       this.showPopupTimer &&
@@ -59,13 +46,37 @@ export default class AddToHomescreen extends Component {
 
   get PWALabel() {
     return I18n.t(themePrefix("pwa_text"), {
-      siteTitle: this.site.siteSettings.title
+      siteTitle: this.site.siteSettings.title,
     });
+  }
+
+  @action
+  setup() {
+    if (this.capabilities.isIpadOS) {
+      this.arrowUp = true;
+      return;
+    }
+
+    window
+      .matchMedia("(orientation: portrait)")
+      .addEventListener("change", this.handleOrientationChange);
+  }
+
+  @action
+  teardown() {
+    window
+      .matchMedia("(orientation: portrait)")
+      .removeEventListener("change", this.handleOrientationChange);
   }
 
   @action
   hidePopup() {
     this.keyValueStore.setItem("hasHiddenPopup", true);
     this.hasHiddenPopup = true;
+  }
+
+  @bind
+  handleOrientationChange(event) {
+    this.arrowUp = event.matches;
   }
 }
